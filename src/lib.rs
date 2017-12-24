70cba63ed601562f83e6a84f57062ddf504bb5ee
dobkeratops <dobkeratopsgit@gmail.com>
2017-12-24 01:07:01 +0000

    ok

--- src/lib.rs
+++ src/lib.rs
@@ -7,14 +7,14 @@ mod tests {
 }
 
 #[derive(Debug,Clone)]
-pub struct Vec1<X>{x:X}
+pub struct Vec1<X>{pub x:X}
 
 #[derive(Debug,Clone)]
-pub struct Vec2<X,Y=X>{x:X,y:Y}
+pub struct Vec2<X,Y=X>{pub x:X,pub y:Y}
 
 #[derive(Debug,Clone)]
-pub struct Vec3<X,Y=X,Z=Y>{x:X,y:Y,z:Z}
+pub struct Vec3<X,Y=X,Z=Y>{pub x:X,pub y:Y,pub z:Z}
 
 #[derive(Debug,Clone)]
-pub struct Vec4<X,Y=X,Z=Y,W=Z>{x:X,y:Y,z:Z,w:W}
+pub struct Vec4<X,Y=X,Z=Y,W=Z>{pub x:X,pub y:Y,pub z:Z,pub w:W}
 
