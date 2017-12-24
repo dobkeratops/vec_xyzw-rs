34529d51f961770198654b90d6ab9b4876282543
dobkeratops <dobkeratopsgit@gmail.com>
2017-12-24 01:34:17 +0000

    ok

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,24 @@
 #[cfg(test)]
 mod tests {
+	use super::*;
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
 }
 
-#[derive(Debug,Clone)]
-pub struct Vec1<X>{pub x:X}
+// dammit.. have to commit to 'Copy' types for current sources :(
 
-#[derive(Debug,Clone)]
-pub struct Vec2<X,Y=X>{pub x:X,pub y:Y}
 
-#[derive(Debug,Clone)]
-pub struct Vec3<X,Y=X,Z=Y>{pub x:X,pub y:Y,pub z:Z}
+#[derive(Debug,Clone,Copy)]
+pub struct Vec1<X:Copy>{pub x:X}
 
-#[derive(Debug,Clone)]
-pub struct Vec4<X,Y=X,Z=Y,W=Z>{pub x:X,pub y:Y,pub z:Z,pub w:W}
+#[derive(Debug,Clone,Copy)]
+pub struct Vec2<X:Copy,Y:Copy=X>{pub x:X,pub y:Y}
+
+#[derive(Debug,Clone,Copy)]
+pub struct Vec3<X:Copy,Y:Copy=X,Z:Copy=Y>{pub x:X,pub y:Y,pub z:Z}
+
+#[derive(Debug,Clone,Copy)]
+pub struct Vec4<X:Copy,Y:Copy=X,Z:Copy=Y,W:Copy=Z>{pub x:X,pub y:Y,pub z:Z,pub w:W}
 
